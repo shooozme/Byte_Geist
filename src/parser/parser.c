11a717dc8e8b3aa1f9252ad7d72ccbcd9fd54639
shooozme <shooozme@gmail.com>
2024-04-26 01:32:57 -0400

    fix: (NOT WORKING) isRegisterValid changed

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include "token.h"
 
+#define REGSIZE 16
+
+//Const array of all valid register names
+//should REG_0 always be zero?
+const char RegNames[REGSIZE][5] = {
+    "REG_0", "REG_1", "REG_2", "REG_3", "REG_4", 
+    "REG_5", "REG_6", "REG_7", "REG_8", "REG_9", 
+    "REG10", "REG11", "REG12", "REG13", "REG14", 
+    "REG15"};
+
 Token* parseData(char *fileInput) {
     //n is thenumber of characters
     int n;
@@ -37,13 +47,12 @@ Token* parseData(char *fileInput) {
     //parsedata needs to terminate when reaching EOL!
 }
 
-int isRegisterValid(char *regName) {
-    char array[15][15] = {"REG_1", "REG_2", "REG_3", "REG_4", "REG_5", "REG_6", "REG_7", "REG_8"};
+unsigned int isRegisterValid(char *regName) {
 
-    for(int i = 0; i < 15; i++) {
-        if(strncomp(array[0], regName, 15) == 0) {
-            //a valid register has been entered return 0
-            return 0;
+    for(int i = 0; i < REGSIZE; i++) {
+        if(strcmp(RegNames[i], regName) == 0) {
+            //a valid register has been entered return 1
+            return 1;
         } else {
             //a nonvalid register has been entered in the file spit out the name, and exit!
             printf("A nonvalid register has been entered %s", regName);
