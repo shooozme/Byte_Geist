e389330387d007ece144f1811675167c03571e0d
shooozme <shooozme@gmail.com>
2024-04-26 01:33:57 -0400

    fix: (NOT WORKING) changed commands to use isRegisterValid

--- src/commands/commands.c
+++ src/commands/commands.c
@@ -1,7 +1,16 @@
-void ALLOCATE() {
+#include <stdio.h>
+#include <parser.c>
 
+void ALLOCATE(char *REG, const unsigned int data) {
+    //isRegisterValid acts as a guard clause
+    isRegistervalid(REG);
+    
 }
 
 void DELETE() {
+
+}
+
+int ADD(char *REG1, char *REG2) {
 
 }
