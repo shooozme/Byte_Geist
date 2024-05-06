1c789cffcb865e5577297bf3a1d1bbbc45d68bb2
Jay <compundbread@gmail.com>
2024-05-05 21:59:40 -0400

    build: add CMake
    Co-Authored-By: shooozme <152763127+shooozme@users.noreply.github.com>

---
+++ CMakeFiles/ByteGeist.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ByteGeist.dir/src/interpreter.c.o"
+  "CMakeFiles/ByteGeist.dir/src/interpreter.c.o.d"
+  "bin/ByteGeist"
+  "bin/ByteGeist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ByteGeist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/ByteGeist.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ByteGeist.
+# This may be replaced when dependencies are built.
